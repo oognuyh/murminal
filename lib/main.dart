0bb9fb0b3a88168617b0a8e6b847a62290bd5ab7
oognuyh <oognuyh@gmail.com>
2026-03-15 13:18:36 +0900

    fix: initialize SharedPreferences in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:murminal/app.dart';
+import 'package:murminal/core/providers.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferences.getInstance();
+
   runApp(
-    const ProviderScope(
-      child: MurminalApp(),
+    ProviderScope(
+      overrides: [
+        sharedPreferencesProvider.overrideWithValue(prefs),
+      ],
+      child: const MurminalApp(),
     ),
   );
 }
